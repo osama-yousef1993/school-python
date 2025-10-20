5ce2e0030511ff30d2f4daadad1b4f5fead27672
Heba Yasin <hebah.yasin@optimumpartners.co>
2025-10-20 12:31:03 +0300

    added error handling

--- src/helpers/file_helpers.py
+++ src/helpers/file_helpers.py
@@ -1,14 +1,20 @@
 import json
 
-
 class FileHelper:
     def __init__(self):
         self.folder_path = "files/"
 
     def read_file(self, file_name):
-        with open(f"{self.folder_path}{file_name}", "r") as file:
-            return json.loads(file.read())
+        try:
+            with open(f"{self.folder_path}{file_name}", "r") as file:
+                return json.loads(file.read())
+        except(FileNotFoundError, json.JSONDecodeError):
+            return []
+
 
     def write_file(self, file_name, content):
-        with open(f"{self.folder_path}{file_name}", "w") as file:
-            file.write(json.dumps(content))
+        try:
+            with open(f"{self.folder_path}{file_name}", "w") as file:
+                file.write(json.dumps(content))
+        except(FileNotFoundError, json.JSONDecodeError):
+            return []
